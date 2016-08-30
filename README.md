0370a09d3718524bcd00c28eef6ebf8eea5c878f
Panu Kalliokoski <panu.kalliokoski@solita.fi>
2016-08-30 15:55:20 +0300

    Add checking script and some usage documentation

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # csv-quality-checker
-A utility script for finding signs of data errors in tabular data (aka data "cubes").
+
+A utility script for finding signs of data errors in tabular data (aka
+data "cubes").
+
+## Usage
+
+python check_csv.py <file.csv>
+
+## Diagnostics
+
+check_csv.py prints diagnostic messages about things that might be signs
+of errors in data.  Here are the explanations for them:
+
+"Warning: field _n_ has a stray value"
+
+The field seems to mostly have values that are from a small set of
+values (an "enumeration" or "codeset"), but this particular value only
+appears once or twice.  Usually a sign of a typo, or some mixup in
+fields.
+
+"Warning: field _n_ has null value(s)"
+
+The field has some kind of value for most rows, but some line(s) don't
+have a value for this field.  Usually a sign that fetching the value for
+this field is failing for some record(s), depending on how the CSV was
+produced.
+
+"Warning: length of field _n_ has a stray value"
+
+The field is usually of some specific length, but has an atypical length
+on some rows.  Usually a sign of a typo, or mixup in fields.
+
+"field _n_ does not seem to have a set of common values"
+"length of field _n_ does not seem to have a set of common values"
+
+Not really a warning, but just a mention that the field seems to be free
+text and so warnings about stray values (or lengths) are being
+suppressed.
+
+"line _n_, multiple errors: ..."
+
+This line caused many warnings.  It is usually a sign that the line is
+scrambled or doesn't come from the same dataset.
+
+"Warning: very few lines"
+
+The file has at most one line.  Proper CSV files tend to have many.
+
+"Warning: field count has a stray value"
+
+CSV files usually have the same number of fields on every line.  Some
+line(s) have a different number of fields than most lines in the file.
+This is usually a sign of a scrambled line, or incorrectly escaped field
+separator.
---
+++ check_csv.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import sys, csv
+
+errors = {}
+#errprint = sys.stderr.write
+
+def main(filename):
+    with open(filename) as f:
+        reader = csv.reader(f, delimiter=';')
+        line_count, line_lens, value_distr, value_len_distr = \
+                read_quality_data(reader)
+    check_quality(line_count, line_lens, value_distr, value_len_distr)
+    multiple_errors()
+
+def read_quality_data(reader):
+    line_count, line_lens, value_distr, value_len_distr = 0, {}, {}, {}
+    for row in reader:
+        line_count += 1
+        line_lens.setdefault(len(row), []).append(line_count)
+        for field in range(len(row)):
+            value_distr.setdefault(field,
+                    {}).setdefault(row[field], []).append(line_count)
+            value_len_distr.setdefault(field,
+                    {}).setdefault(len(row[field]), []).append(line_count)
+    return (line_count, line_lens, value_distr, value_len_distr)
+
+def check_quality(line_count, line_lens, value_distr, value_len_distr):
+    if line_count <= 1: warn("very few lines", line_count, [])
+    check_freqs(line_lens, line_count, "field count")
+    for field in value_distr:
+        check_freqs(value_distr[field], line_count,
+                "field %d" % (field+1))
+    for field in value_len_distr:
+        check_freqs(value_len_distr[field], line_count,
+                "length of field %d" % (field+1))
+
+def check_freqs(distr, total, target):
+    nulls = len(distr.get('', []))
+    if nulls > 0 and nulls < total * .5:
+        warn(target + " has null value(s)", nulls, distr[''])
+    if seems_enumerated(distr):
+        for value in distr:
+            if len(distr[value]) < 3:
+                warn(target + " has a stray value", value, distr[value])
+    else: print(target + " does not seem to have a set of common values")
+
+def seems_enumerated(distr):
+    freqs = sorted(len(v) for v in distr.values())
+    total = sum(freqs)
+    if total < 15: return false
+    most_common_total = sum(freqs[-10:])
+    return most_common_total > total * .9
+
+def warn(issue, value, lines):
+    for line in lines:
+        errors.setdefault(line, []).append(issue)
+    if len(lines) > 5: lines = lines[:5] + ['...']
+    print("Warning: %s (%s), on lines: %s" %
+            (issue, str(value), ', '.join(str(l) for l in lines)))
+
+def multiple_errors():
+    for l in sorted(errors.keys()):
+        if len(errors[l]) > 1:
+            print("line %d, multiple errors: %s" % (l, ', '.join(errors[l])))
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
